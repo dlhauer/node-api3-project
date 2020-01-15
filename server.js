c4762ea1f3268b8e444c0e3b8f23439778536e0b
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-15 12:00:02 -0800

    Logger function

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
 
 const server = express();
-server.use(express.json())
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('/api/users', userRouter);
-server.use('/api/posts', postRouter);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  console.log(`[${new Date().toISOString()}] : ${method} to ${originalUrl}`);
+  next();
+}
 
 module.exports = server;
