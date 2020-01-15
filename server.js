3bcc36a4c2fc26ffb81c828fc65d82710476ceeb
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-15 11:55:06 -0800

    Boilerplate stuff

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 //custom middleware
 
