2339a9f012af28046d6f3a737c6bfbc079961a56
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-16 12:29:39 -0800

    Configured for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server');
+const port = process.env.PORT || 6000;
 
-
-server.listen(5000, () => {
-  console.log('\n *** Server running on http://localhost:5000 ***\n')
-})
+server.listen(port, () => {
+  console.log(`\n *** Server running on http://localhost:${port} ***\n`)
+});
