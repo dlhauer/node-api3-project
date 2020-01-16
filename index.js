175dae7b3176ef435be5353534454f685a73870a
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-16 12:55:59 -0800

    Added environment variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
-const port = process.env.PORT || 6000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`\n *** Server running on http://localhost:${port} ***\n`)
